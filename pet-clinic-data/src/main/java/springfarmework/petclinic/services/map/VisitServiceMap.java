b520acc20d5c2e486e00fa08c442758ee0c180a1
Jose <jldmp2001@gmail.com>
2022-09-07 13:28:46 +0200

    Implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/springfarmework/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package springfarmework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import springfarmework.petclinic.model.Visit;
+import springfarmework.petclinic.services.VisitService;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+                throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
